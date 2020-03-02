c50e097db8cc80956135403fb5e2444bcf6ac25a
Ксения Храмина <hramina-ks@hotmail.com>
2020-03-02 23:27:29 +0300

    Проект

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace familyFinance
+{
+    partial class frmFinance
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIncomeAdd = new System.Windows.Forms.TextBox();
+            this.numIncomeDay = new System.Windows.Forms.NumericUpDown();
+            this.cmbIncomeArticles = new System.Windows.Forms.ComboBox();
+            this.btnIncomeAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtExpenseAdd = new System.Windows.Forms.TextBox();
+            this.numExpenseDay = new System.Windows.Forms.NumericUpDown();
+            this.cmbExpenseArticles = new System.Windows.Forms.ComboBox();
+            this.btnExpenseAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numIncomeDay)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numExpenseDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(13, 13);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(13, 43);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Статья:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "День:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Значение:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIncomeAdd);
+            this.groupBox1.Controls.Add(this.numIncomeDay);
+            this.groupBox1.Controls.Add(this.cmbIncomeArticles);
+            this.groupBox1.Controls.Add(this.btnIncomeAdd);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(11, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 147);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Доходы:";
+            // 
+            // txtIncomeAdd
+            // 
+            this.txtIncomeAdd.Location = new System.Drawing.Point(70, 76);
+            this.txtIncomeAdd.Name = "txtIncomeAdd";
+            this.txtIncomeAdd.Size = new System.Drawing.Size(100, 22);
+            this.txtIncomeAdd.TabIndex = 9;
+            // 
+            // numIncomeDay
+            // 
+            this.numIncomeDay.Location = new System.Drawing.Point(70, 48);
+            this.numIncomeDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numIncomeDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numIncomeDay.Name = "numIncomeDay";
+            this.numIncomeDay.Size = new System.Drawing.Size(43, 22);
+            this.numIncomeDay.TabIndex = 8;
+            this.numIncomeDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cmbIncomeArticles
+            // 
+            this.cmbIncomeArticles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbIncomeArticles.FormattingEnabled = true;
+            this.cmbIncomeArticles.Location = new System.Drawing.Point(70, 21);
+            this.cmbIncomeArticles.Name = "cmbIncomeArticles";
+            this.cmbIncomeArticles.Size = new System.Drawing.Size(120, 21);
+            this.cmbIncomeArticles.TabIndex = 7;
+            // 
+            // btnIncomeAdd
+            // 
+            this.btnIncomeAdd.Location = new System.Drawing.Point(50, 111);
+            this.btnIncomeAdd.Name = "btnIncomeAdd";
+            this.btnIncomeAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnIncomeAdd.TabIndex = 6;
+            this.btnIncomeAdd.Text = "Добавить";
+            this.btnIncomeAdd.UseVisualStyleBackColor = true;
+            this.btnIncomeAdd.Click += new System.EventHandler(this.btnIncomeAdd_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtExpenseAdd);
+            this.groupBox2.Controls.Add(this.numExpenseDay);
+            this.groupBox2.Controls.Add(this.cmbExpenseArticles);
+            this.groupBox2.Controls.Add(this.btnExpenseAdd);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(246, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(207, 147);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Расходы:";
+            // 
+            // txtExpenseAdd
+            // 
+            this.txtExpenseAdd.Location = new System.Drawing.Point(70, 76);
+            this.txtExpenseAdd.Name = "txtExpenseAdd";
+            this.txtExpenseAdd.Size = new System.Drawing.Size(100, 22);
+            this.txtExpenseAdd.TabIndex = 9;
+            // 
+            // numExpenseDay
+            // 
+            this.numExpenseDay.Location = new System.Drawing.Point(70, 48);
+            this.numExpenseDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numExpenseDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numExpenseDay.Name = "numExpenseDay";
+            this.numExpenseDay.Size = new System.Drawing.Size(43, 22);
+            this.numExpenseDay.TabIndex = 8;
+            this.numExpenseDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cmbExpenseArticles
+            // 
+            this.cmbExpenseArticles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbExpenseArticles.FormattingEnabled = true;
+            this.cmbExpenseArticles.Location = new System.Drawing.Point(70, 21);
+            this.cmbExpenseArticles.Name = "cmbExpenseArticles";
+            this.cmbExpenseArticles.Size = new System.Drawing.Size(120, 21);
+            this.cmbExpenseArticles.TabIndex = 7;
+            // 
+            // btnExpenseAdd
+            // 
+            this.btnExpenseAdd.Location = new System.Drawing.Point(50, 111);
+            this.btnExpenseAdd.Name = "btnExpenseAdd";
+            this.btnExpenseAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnExpenseAdd.TabIndex = 6;
+            this.btnExpenseAdd.Text = "Добавить";
+            this.btnExpenseAdd.UseVisualStyleBackColor = true;
+            this.btnExpenseAdd.Click += new System.EventHandler(this.btnExpenseAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Значение:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "День:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Статья:";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(11, 235);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(442, 188);
+            this.rtbOutput.TabIndex = 8;
+            this.rtbOutput.Text = "";
+            this.rtbOutput.WordWrap = false;
+            // 
+            // frmFinance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmFinance";
+            this.Text = "Семейный бюджет";
+            this.Load += new System.EventHandler(this.frmFinance_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numIncomeDay)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numExpenseDay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtIncomeAdd;
+        private System.Windows.Forms.NumericUpDown numIncomeDay;
+        private System.Windows.Forms.ComboBox cmbIncomeArticles;
+        private System.Windows.Forms.Button btnIncomeAdd;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtExpenseAdd;
+        private System.Windows.Forms.NumericUpDown numExpenseDay;
+        private System.Windows.Forms.ComboBox cmbExpenseArticles;
+        private System.Windows.Forms.Button btnExpenseAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace familyFinance
+{
+    public partial class frmFinance : Form
+    {
+        public frmFinance()
+        {
+            InitializeComponent();
+        }
+
+        static string[] sIncomeArticles = new string[] { "Осн.доход.мужа\t", "Осн.доход.жены\t", "Доп.доход.мужа\t", "Доп.доход.жены\t", "Подарки\t\t", "Соц.выплаты\t" };
+        static string[] sExpenseArticles = new string[] { "Еда\t\t", "Жилье\t\t", "Одежда\t\t", "Транспорт\t", "Связь\t\t", "Обучение\t", "Развлечения\t", "Путешествия\t", "Благотворительность", "Прочее\t\t", };
+        /* Символ \t в строковых переменных обозначает абзац, он необходим, чтобы выровнять поля и сделать их визуально красивыми при выводе. В названии статей не должно быть пробелов! 
+         * Это должны быть целые слова, потому что пробел будет служить нам разделителем при чтении из файла, чтобы отделять один элемент от другого */
+        public class Fields
+        {
+            /*Переменные, отвечающие за количество используемых строк и столбцов из массива Articles*/
+            public int Rows, Columns = 32;
+            /*Создаем двумерный массив, в котором будут храниться данные по каждой статье и дню. Задаем 50 строк, чтобы можно было создавать много статей (на перспективу) */
+            public string[,] Articles = new string[50, 32];
+            /*Конструктор класса, в котором мы сразу при создании объекта определяем количество используемых строк из массива Articles */
+            public Fields(int rows)
+            {
+                Rows = rows;
+            }
+
+            /*Метод, который задает значения первого столбца массива Articles из передаваемого в него массива*/
+            public void SetFirstColumn(string[] mas)
+            {
+                Articles[0, 0] = "Статья/День\t";
+                for (int i = 1; i < Rows; i++)
+                {
+                    Articles[i, 0] = mas[i - 1];
+                }
+            }
+
+            /* Метод, который присваивает значениям первой строки массива Articles значения дней месяца */
+            public void SetDayNumbers()
+            {
+                for (int i = 1; i <= Columns - 1; i++)
+                    Articles[0, i] = Convert.ToString(i);
+            }
+
+            // Метод, который построчно выводит все значения массива Articles 
+            public string Output()
+            {
+                string str = "";
+                for (int i = 0; i < Rows; i++)
+                {
+                    for (int j = 0; j < Columns; j++)
+                    {
+                        if (Articles[i, j] != null)
+                            str = str + Articles[i, j] + " \t";
+                        else
+                            str = str + "- \t";
+                    }
+                    str = str + "\r\n";
+                }
+                return str;
+            }
+
+            //Метод, который считает сумму всех элементов массива Articles
+            public int SumOfElements()
+            {
+                int iSum = 0;
+                for (int i = 1; i < Rows; i++)
+                    for (int j = 1; j < Columns; j++)
+                        if (Articles[i, j] != "" && Articles[i, j] != "-")
+                            iSum = iSum + Convert.ToInt32(Articles[i, j]);
+                return iSum;
+            }
+        }
+
+        //Функция для строковой переменной
+        string TextBoxLine(string sIncome, string sAllIncome, string sExpense, string sAllExpense, string sDifference)
+        {
+            string str = "";
+            str = str + "Доходы:\r\n" + sIncome;
+            str = str + "Общий доход: " + sAllIncome + "\r\n\r\n";
+            str = str + "Расходы:\r\n" + sExpense;
+            str = str + "Общий расход: " + sAllExpense + "\r\n\r\n";
+            str = str + "В наличии: " + sDifference;
+            return str;
+        }
+
+        Fields Income = new Fields(sIncomeArticles.Length + 1);
+        Fields Expense = new Fields(sExpenseArticles.Length + 1);
+        /*Передаем количество строк на одну больше, потому что кроме названия статей у нас есть еще первая строка, в которой будут дни*/
+
+        private void frmFinance_Load(object sender, EventArgs e)
+        {
+            /*Вызываем методы, которые заполняют первые столбцы значениями из массивов sIncomeArticles и sExpenseArticles*/
+            Income.SetFirstColumn(sIncomeArticles);
+            Expense.SetFirstColumn(sExpenseArticles);
+
+            /*Вызываем методы, которые заполняют первые строки значениями дней*/
+            Income.SetDayNumbers();
+            Expense.SetDayNumbers();
+
+            /*Заполняем выпадающие списки элементами из массива Articles объектов Income и Expense*/
+            for (int i = 0; i < Income.Rows - 1; i++)
+                cmbIncomeArticles.Items.Add(Income.Articles[i + 1, 0]);
+            for (int i = 0; i < Expense.Rows - 1; i++)
+                cmbExpenseArticles.Items.Add(Expense.Articles[i + 1, 0]);
+
+            /*Выводим в окно вывода начальные значения доходов и расходов, используя функцию TextBoxLine*/
+            rtbOutput.Text = TextBoxLine(Income.Output(), Convert.ToString(Income.SumOfElements()), Expense.Output(), Convert.ToString(Expense.SumOfElements()), Convert.ToString(Income.SumOfElements() - Expense.SumOfElements()));
+        }
+
+        private void btnIncomeAdd_Click(object sender, EventArgs e)
+        {
+            //Проверка, что пользователь выбрал что-то из списка
+            if (cmbIncomeArticles.SelectedIndex != -1)
+            {
+                //Проверка, что пользователь ввел значение в окно
+                if (txtIncomeAdd.Text != "")
+                {
+                    //Проверка, что перезаписываемое значение не "-"
+                    if (Income.Articles[(cmbIncomeArticles.SelectedIndex + 1), Convert.ToInt32(numIncomeDay.Value)] != "-")
+                        Income.Articles[(cmbIncomeArticles.SelectedIndex + 1), Convert.ToInt32(numIncomeDay.Value)] = Convert.ToString(Convert.ToInt32(Income.Articles[(cmbIncomeArticles.SelectedIndex + 1), Convert.ToInt32(numIncomeDay.Value)]) + Convert.ToInt32(txtIncomeAdd.Text));
+                    /*Обновляем значение поля массива Articles, прибавляя к нему значение из текстового поля. Номера элементов берем из выпадающего списка и списка дней месяца. 
+                     Делаем явные конвертации типов, чтобы не возникало ошибок.*/
+                    /*Если в массиве был "-", то просто записываем данные из поля*/
+                    else
+                        Income.Articles[(cmbIncomeArticles.SelectedIndex + 1), Convert.ToInt32(numIncomeDay.Value)] = txtIncomeAdd.Text;
+                    /*Просто записываем значение из текстового поля в элемент массива Articles. Обновляем окно вывода данных, заново вызывая функцию*/
+                    rtbOutput.Text = TextBoxLine(Income.Output(), Convert.ToString(Income.SumOfElements()), Expense.Output(), Convert.ToString(Expense.SumOfElements()), Convert.ToString(Income.SumOfElements() - Expense.SumOfElements()));
+                    MessageBox.Show("Статья доходов добавлена");
+                }
+
+                else
+                {
+                    MessageBox.Show("Введите значение дохода");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Выберите статью дохода");
+            }
+        }
+
+        private void btnExpenseAdd_Click(object sender, EventArgs e)
+        {
+            if (cmbExpenseArticles.SelectedIndex != -1)
+            {
+                if (txtExpenseAdd.Text != "")
+                {
+                    if (Expense.Articles[(cmbExpenseArticles.SelectedIndex + 1), Convert.ToInt32(numExpenseDay.Value)] != "-")
+                        Expense.Articles[(cmbExpenseArticles.SelectedIndex + 1), Convert.ToInt32(numExpenseDay.Value)] = Convert.ToString(Convert.ToInt32(Expense.Articles[(cmbExpenseArticles.SelectedIndex + 1), Convert.ToInt32(numExpenseDay.Value)]) + Convert.ToInt32(txtExpenseAdd.Text));
+                    else
+                        Expense.Articles[(cmbExpenseArticles.SelectedIndex + 1), Convert.ToInt32(numExpenseDay.Value)] = txtExpenseAdd.Text;
+                    rtbOutput.Text = TextBoxLine(Income.Output(), Convert.ToString(Income.SumOfElements()), Expense.Output(), Convert.ToString(Expense.SumOfElements()), Convert.ToString(Income.SumOfElements() - Expense.SumOfElements()));
+                    MessageBox.Show("Статья расходов добавлена");
+                }
+                else
+                    MessageBox.Show("Введите значение расхода");
+            }
+            else
+                MessageBox.Show("Выберите статью расхода");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream fsFile = new FileStream("Семейные финансы.txt", FileMode.Create);
+            StreamWriter fileWrite = new StreamWriter(fsFile);
+            //Записываем данные в файл, вызывая функцию TextBoxLine
+            fileWrite.Write(TextBoxLine(Income.Output(), Convert.ToString(Income.SumOfElements()), Expense.Output(), Convert.ToString(Expense.SumOfElements()), Convert.ToString(Income.SumOfElements() - Expense.SumOfElements())));
+            fileWrite.Close();
+            MessageBox.Show("Данные сохранены в файл");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //создаем объект для подключения к файлу
+            FileStream fsFile = new FileStream("Семейные финансы.txt", FileMode.Open);
+            try
+            {
+                //создаем объект для чтения из файла
+                using (StreamReader fileRead = new StreamReader(fsFile))
+                {
+                    string temp;
+                    //читаем первую строку - "доходы" - нам не нужна
+                    temp = fileRead.ReadLine();
+                    //цикл прохода по всем строкам первого блока (Доходы)
+                    for (int i = 0; i < Income.Rows; i++)
+                    {
+                        //переменная, в которой указан разделитель
+                        string[] separator = new string[] { " " };
+                        //временный массив, в который будет записываться строка целиком
+                        string[] mas = new string[Income.Columns];
+                        //записываем временный массив, отделяем переменные друг от друга по разделителю
+                        mas = fileRead.ReadLine().Split(separator, StringSplitOptions.None);
+                        /*Цикл записи данных из временного массива в массив Articles, так же перед записью убираем значения абзацев,
+                         которые были во временном массиве*/
+                        for (int j = 0; j < Income.Columns; j++)
+                            Income.Articles[i, j] = mas[j].Trim(new char[] { '\t' });
+                    }
+                    //читаем лишнее (общий доход)
+                    temp = fileRead.ReadLine();
+                    //читаем лишнее (пустая строка)
+                    temp = fileRead.ReadLine();
+                    //читаем лишнее (расходы)
+                    temp = fileRead.ReadLine();
+                    //цикл прохода по всем строкам второго блока (Расходы)
+                    for (int i = 0; i < Expense.Rows; i++)
+                    {
+                        string[] separator = new string[] { " " };
+                        string[] mas = new string[Expense.Columns];
+                        mas = fileRead.ReadLine().Split(separator, StringSplitOptions.None);
+                        for (int j = 0; j < Expense.Columns; j++)
+                            Expense.Articles[i, j] = mas[j].Trim(new char[] { '\t' });
+                    }
+                    fileRead.Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Файл не может быть прочитан");
+            }
+            /*Поскольку мы убирали значения абзацев, для красоты их надо вернуть.
+             Для этого мы просто заново перезаписываем первые колонки массивов, вызывая метод SetFirstColumn*/
+            Income.SetFirstColumn(sIncomeArticles);
+            Expense.SetFirstColumn(sExpenseArticles);
+            //Обновляем данные в окне вывода, вызывая функцию TextBoxLine
+            rtbOutput.Text = TextBoxLine(Income.Output(), Convert.ToString(Income.SumOfElements()), Expense.Output(), Convert.ToString(Expense.SumOfElements()), Convert.ToString(Income.SumOfElements() - Expense.SumOfElements()));
+            MessageBox.Show("Данные загружены из файла");
+        }
+    }
+}
